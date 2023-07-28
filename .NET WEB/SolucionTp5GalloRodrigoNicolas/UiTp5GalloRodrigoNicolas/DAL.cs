b2214d5f5db5d63a90af77949f0c83db3092a377
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2023-07-28 09:07:44 -0300

    TP5

---
+++ .NET WEB/SolucionTp5GalloRodrigoNicolas/UiTp5GalloRodrigoNicolas/DAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UiTp5GalloRodrigoNicolas
+{
+    public class DAL
+    {
+
+        public void Conectar(string queryString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString: ConfigurationManager.ConnectionStrings["Cnn"].ToString()))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.CommandType = CommandType.Text;
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+                
+            }
+
+        }      
+        
+
+    }
+
+}
---
+++ .NET WEB/SolucionTp5GalloRodrigoNicolas/UiTp5GalloRodrigoNicolas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UiTp5GalloRodrigoNicolas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string create = "INSERT INTO Usuarios (Id, Nombre, Apellido,Edad) VALUES (1, ´Alan´, ´Roman´, 11)";
+            string create2 = "INSERT INTO Usuarios (Id, Nombre, Apellido,Edad) VALUES (2, ´Rodrigo´, ´Gallo´, 28);";
+            string create3 = "INSERT INTO Usuarios (Id, Nombre, Apellido,Edad) VALUES (3, ´Nicolas´, ´Fernandez´, 33);";
+
+            string consulta = "Select * From Usuarios;";
+
+            Console.ReadLine();
+
+            DAL dal = new DAL();
+            dal.Conectar(create);
+            //dal.Conectar(create2);
+            //dal.Conectar(create3);
+
+            //dal.Conectar(create);
+
+
+        }
+    }
+}
